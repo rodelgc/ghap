fb179fda5f7ac1594755b8d376e7d0c17c2ce6d6
rodel <rodel.calasagsag@automattic.com>
2022-04-06 16:01:16 +0800

    Implement test type

---
+++ .github/workflows/scripts/push-changes.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#
+# Pushes the newly generated report to the repo.
+#
+
+COMMIT_MESSAGE=""
+
+set_commit_message() {
+
+    case $TEST_TYPE in
+    "e2e")
+        COMMIT_MESSAGE="Added: E2E test report"
+        ;;
+    "api")
+        COMMIT_MESSAGE="Added: API test report"
+        ;;
+    esac
+
+    case $TEST_WORKFLOW in
+    "daily")
+        COMMIT_MESSAGE="$COMMIT_MESSAGE -- Daily build"
+        ;;
+    "pr")
+        COMMIT_MESSAGE="$COMMIT_MESSAGE -- PR $PR_NUMBER"
+        ;;
+    "release")
+        COMMIT_MESSAGE="$COMMIT_MESSAGE -- Release build"
+        ;;
+    esac
+
+    COMMIT_MESSAGE="$COMMIT_MESSAGE -- Run $RUN_ID"
+}
+
+set_commit_message
+
+cd $REPO_PATH
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add .
+git commit -m "$COMMIT_MESSAGE"
+git push
--- .github/workflows/scripts/regenerate-report.sh
+++ .github/workflows/scripts/regenerate-report.sh
@@ -5,29 +5,98 @@
 # This will create history trend if previous reports exist.
 #
 
-# Folder in the repo where new report will be generated.
-PR_REPORT_PATH="$REPO_PATH/pr/$PR_NUMBER"
+# The root dir of the report, whether it be the "e2e" or "api" root folder.
+ROOT_DIR=""
 
-# Folder in the repo containing all previous files of allure-results.
-# Contents of allure-results folder from the new report will be added here.
-PR_RESULTS_PATH="$REPO_PATH/pr/$PR_NUMBER-results"
+# Folder in the repo where the new report (containing history trend) will be generated.
+REPORT_PATH=""
 
-if [[ -d "$PR_REPORT_PATH" ]]; then
+# Folder that contains all previous results.
+# Contents of newly downloaded "allure-results" folder will be placed here.
+RESULTS_PATH=""
 
-    # PR has previous reports.
-    # Combine them with downloaded test report to create history trend.
-    mkdir -p $PR_RESULTS_PATH/history
-    cp -r $DOWNLOAD_PATH/allure-report/history/* $PR_RESULTS_PATH/history
-    cp -r $DOWNLOAD_PATH/allure-results/* $PR_RESULTS_PATH
+# Title of the report as viewed in the browser tab and in the Overview page.
+REPORT_TITLE=""
+
+# Sets up values for env vars above, depending on the provided TEST_TYPE and TEST_WORKFLOW.
+set_paths() {
+
+    # Determine whether report will be generated in `e2e` or `api` root folder.
+    case $TEST_TYPE in
+    'e2e')
+        ROOT_DIR="$REPO_PATH/e2e"
+        REPORT_TITLE="WooCommerce E2E"
+        ;;
+    'api')
+        ROOT_DIR="$REPO_PATH/api"
+        REPORT_TITLE="WooCommerce REST API"
+        ;;
+    *)
+        echo "You provided a wrong value for TEST_TYPE: \"$TEST_TYPE\""
+        echo "Options are: [ e2e | api ]"
+        echo "Exiting with code 1."
+        exit 1
+        ;;
+    esac
+
+    # Determine whether report will be in daily, pr, or release folder
+    case $TEST_WORKFLOW in
+    'daily')
+        REPORT_PATH="$ROOT_DIR/daily"
+        RESULTS_PATH="$ROOT_DIR/daily-results"
+        REPORT_TITLE="$REPORT_TITLE - Daily Builds"
+        ;;
+    'pr')
+        REPORT_PATH="$ROOT_DIR/pr/$PR_NUMBER"
+        RESULTS_PATH="$ROOT_DIR/pr/$PR_NUMBER-results"
+        REPORT_TITLE="$REPORT_TITLE - PR $PR_NUMBER"
+        ;;
+    'release')
+        REPORT_PATH="$ROOT_DIR/release"
+        RESULTS_PATH="$ROOT_DIR/release-results"
+        REPORT_TITLE="$REPORT_TITLE - Releases"
+        ;;
+
+    *)
+        echo "You provided a wrong value for TEST_WORKFLOW: \"$TEST_WORKFLOW\""
+        echo "Options are: [ daily | pr | release ]"
+        echo "Exiting with code 1."
+        exit 1
+        ;;
+    esac
+
+}
+
+# Provides an appropriate title, instead of the default "Allure Report".
+set_report_title() {
+
+    # HTML Title
+    sed -i "s/Allure Report/$REPORT_TITLE/g" $REPORT_PATH/index.html
+
+    # Overview page
+    sed -i "s/Allure Report/$REPORT_TITLE/g" $REPORT_PATH/widgets/summary.json
+}
+
+set_paths
+
+if [[ -d "$REPORT_PATH" ]]; then
+
+    # The test has previous reports.
+    # Combine them with newly downloaded test report to create history trend.
+    mkdir -p $RESULTS_PATH/history
+    cp -r $DOWNLOAD_PATH/allure-report/history/* $RESULTS_PATH/history
+    cp -r $DOWNLOAD_PATH/allure-results/* $RESULTS_PATH
 
 else
 
-    # Create PR's first test report.
-    mkdir -p $PR_REPORT_PATH
-    mkdir -p $PR_RESULTS_PATH
-    cp -r $DOWNLOAD_PATH/allure-results/* $PR_RESULTS_PATH
+    # Create the first report.
+    mkdir -p $REPORT_PATH
+    mkdir -p $RESULTS_PATH
+    cp -r $DOWNLOAD_PATH/allure-results/* $RESULTS_PATH
 
 fi
 
 # Regenerate the report.
-allure generate --clean $PR_RESULTS_PATH --output $PR_REPORT_PATH
+allure generate --clean $RESULTS_PATH --output $REPORT_PATH
+
+set_report_title
