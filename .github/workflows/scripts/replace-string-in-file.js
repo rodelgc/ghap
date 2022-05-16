4654dce16c9c6d5f87481205ba416f75007c0ef4
rodel <rodel.calasagsag@automattic.com>
2022-05-16 17:22:35 +0800

    Cleanup

--- .github/workflows/scripts/replace-string-in-file.js
+++ .github/workflows/scripts/replace-string-in-file.js
@@ -5,17 +5,6 @@ const file = args[0];
 const searchValue = args[1];
 const replacement = args[2];
 
-// readFile(file, "utf8", (err, fileContent) => {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   const newFileContent = fileContent.replace(searchValue, replacement);
-
-//   writeFile(file, newFileContent, "utf8", (err) => {
-//     if (err) return console.log(err);
-//   });
-// });
-
 const content = readFileSync(file);
 const newFileContent = content.toString().replace(searchValue, replacement);
 
