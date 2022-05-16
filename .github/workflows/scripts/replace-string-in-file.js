e22396e8a7a1fdba3afe658da229d0e9f5c81631
rodel <rodel.calasagsag@automattic.com>
2022-05-16 17:04:12 +0800

    Use sync'ed methods

--- .github/workflows/scripts/replace-string-in-file.js
+++ .github/workflows/scripts/replace-string-in-file.js
@@ -1,17 +1,22 @@
-const { readFile, writeFile } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 
 const args = process.argv.slice(2);
 const file = args[0];
 const searchValue = args[1];
 const replacement = args[2];
 
-readFile(file, "utf8", (err, fileContent) => {
-  if (err) {
-    return console.log(err);
-  }
-  const newFileContent = fileContent.replace(searchValue, replacement);
+// readFile(file, "utf8", (err, fileContent) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   const newFileContent = fileContent.replace(searchValue, replacement);
 
-  writeFile(file, newFileContent, "utf8", (err) => {
-    if (err) return console.log(err);
-  });
-});
+//   writeFile(file, newFileContent, "utf8", (err) => {
+//     if (err) return console.log(err);
+//   });
+// });
+
+const content = readFileSync(file);
+const newFileContent = content.replace(searchValue, replacement);
+
+writeFileSync(file, newFileContent);
