abf070e0fd500456b6dc7ab436e3836ebf32c6db
rodel <rodel.calasagsag@automattic.com>
2022-05-16 16:14:15 +0800

    Try replacing file contents with node

---
+++ .github/workflows/scripts/replace-string-in-file.js
@@ -0,0 +1,15 @@
+import { readFile, writeFile } from "fs";
+
+const someFile = "text.txt";
+const replacement = "Merge branch 'trunk' into e2e/rep-gh-pages";
+
+readFile(someFile, "utf8", (err, fileContent) => {
+  if (err) {
+    return console.log(err);
+  }
+  const newFileContent = fileContent.replace(/Allure Report/g, replacement);
+
+  writeFile(someFile, newFileContent, "utf8", (err) => {
+    if (err) return console.log(err);
+  });
+});
