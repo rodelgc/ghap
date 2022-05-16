24f0a6d552bc84546ad8320875c523ae5113da28
rodel <rodel.calasagsag@automattic.com>
2022-05-16 16:39:49 +0800

    Test special chars in replacement text

--- .github/workflows/scripts/replace-string-in-file.js
+++ .github/workflows/scripts/replace-string-in-file.js
@@ -1,17 +1,17 @@
 const { readFile, writeFile } = require("fs");
 
 const args = process.argv.slice(2);
-const someFile = args[0];
+const file = args[0];
 const searchValue = args[1];
 const replacement = args[2];
 
-readFile(someFile, "utf8", (err, fileContent) => {
+readFile(file, "utf8", (err, fileContent) => {
   if (err) {
     return console.log(err);
   }
   const newFileContent = fileContent.replace(searchValue, replacement);
 
-  writeFile(someFile, newFileContent, "utf8", (err) => {
+  writeFile(file, newFileContent, "utf8", (err) => {
     if (err) return console.log(err);
   });
 });
